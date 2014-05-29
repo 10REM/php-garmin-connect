1b3555173dca9abbe3bfe053434885242004d4d1
David Wilcock <dave.wilcock@gmail.com>
2014-05-29 23:21:07 +0100

    Updated example to be usable resulting from composer update

--- examples/example.php
+++ examples/example.php
@@ -1,16 +1,19 @@
 <?php
-require_once '../vendors/autload.php';
+require_once '../vendor/autoload.php';
 
 $arrCredentials = array(
 'username' => 'xxx',
 'password' => 'xxx'
 );
 
-$objGarminConnect = new GarminConnect($arrCredentials);
+try {
+   $objGarminConnect = new \dawguk\GarminConnect($arrCredentials);
 
-$objResults = $objGarminConnect->getActivityList(0, 1);
-foreach($objResults->results->activities as $objActivity) {
-$intActivityId = (int)$objActivity->activity->activityId;
-$strTCXUrl = "http://connect.garmin.com/proxy/activity-service-1.1/tcx/activity/" . $intActivityId . "?full=true";
-file_put_contents("/tmp/" . $intActivityId . ".tcx", fopen($strTCXUrl, "r"));
+   $objResults = $objGarminConnect->getActivityList(0, 1);
+   foreach($objResults->results->activities as $objActivity) {
+      print_r($objActivity->activity);
+   }
+
+} catch (Exception $objException) {
+   echo "Oops: " . $objException->getMessage();
 }
